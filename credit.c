ce24e830b66b305c3998ed9ff83493d304b97e57
venzom <venzom@users.noreply.github.com>
2021-02-10 10:47:21 +0000

    automated commit by check50 [check50=True]

---
+++ credit.c
@@ -0,0 +1,41 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+//#include <stlib.h>
+
+int main(void)
+{
+    // Get change owed by user
+    long n;
+    do
+    {
+    n = get_long("Number: ");
+    }
+    while (n < 999999999999 || n > 10000000000000000);
+    
+    if (n > 4000000000000 && n <= 4999999999999)
+    {
+        printf("Visa\n");
+    }
+    else if (n > 3400000000000 && n < 380000000000000)
+    {
+        printf("AMEX\n");
+    }
+    else if (n > 40000000000000 && n <= 4999999999999999)
+    {
+        printf("Visa\n");
+    }
+    else if (n > 51000000000000 && n < 5600000000000000)
+    {
+        printf("Mastercard\n");
+    }
+    else
+    {
+        printf("Invalid");
+    }
+    
+}
+
+
+  
