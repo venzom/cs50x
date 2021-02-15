2c05baa48f45bdbe14b2ffd423450b4237dc38e1
venzom <venzom@users.noreply.github.com>
2021-02-15 00:38:08 +0000

    automated commit by submit50

--- caesar.c
+++ caesar.c
@@ -20,7 +20,7 @@ int main(int argc, string argv[])
         return 1;
     }
     
-    // Get plaintext input from user and convert it to cypher using key
+    // Get plaintext input from user and convert it to cipher using key.
     string plain = get_string("plaintext:  ");
     int n = strlen(plain);
     int cipher;
@@ -37,30 +37,7 @@ int main(int argc, string argv[])
             cipher = (((((int)plain[i] - 65) + 26) + key) % 26) + 65;
             printf("%c", (char)cipher);
         }
-            /*cipher = (int)plain[i] + key;
-            //cipher[i] = (plain[i] + key) % 26;
-            if (cipher > 122)
-            {
-                cipher = cipher - 122 + 96;    
-            }
-            else if (cipher > 90 && cipher < 97)
-            {
-                cipher = (cipher % 26) + 64;    
-            }
-            
-        if ((plain[i] >= 'a' && plain[i] <= 'z') || (plain[i] >= 'A' && plain[i] <= 'Z'))
-        {
-            cipher = (int)plain[i] + key;
-            //cipher[i] = (plain[i] + key) % 26;
-            if ((cipher > 122) && (plain[i] >= 'a' && plain[i] <= 'z'))
-            {
-                cipher = cipher - 122 + 96;    
-            }
-            else if ((cipher > 90) && (plain[i] >= 'A' && plain[i] <= 'Z'))
-            {
-                cipher = cipher - 90 + 64;    
-            }*/
-            
+        // Print as is if it is not a letter. 
         else 
         {
             cipher = (int)plain[i];
