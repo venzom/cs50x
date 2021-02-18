eda588f0469f3c8614eab82f88d4cc7ecc720ee2
venzom <venzom@users.noreply.github.com>
2021-02-18 09:42:08 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -2,41 +2,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
-    
+
     // Check to make sure user provides at least 1, but no more than, command-line argument(CLA)
     if (argc == 1 || argc != 2)
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
-    
+
     // Check to make sure "key" CLA is exactly 26 characters
     int len = strlen(argv[1]);
     if (len != 26)
     {
         printf("Key must containg 26 characters.\n");
         return 1;
-    } 
-    
-    
+    }
+
+
     // Make sure "key" CLA only contains alphabetic characters and doesn't contain repeated characters
     string key = argv[1];
     char upp = 'A', low = 'a';
+    char uppAr[26], lowAr[26];
     int count = 0;
     for (int i = 0; i < len; i++)
     {
         if (!(key[i] >= 'a' && key[i] <= 'z' ) && !(key[i] >= 'A' && key[i] <= 'Z'))
         {
             printf("Key must only contain alphabetic characters.\n");
-            return 1; 
+            return 1;
         }
         else if (i != count)
         {
             printf("Key must not contain repeated characters.\n");
-            return 1; 
+            return 1;
         }
         //printf("%c %c %i %i\n", low, key[i], count, i);
         for (int j = 0; j < len; j++)
@@ -46,45 +48,45 @@ int main(int argc, string argv[])
                 count++;
             }
         }
+        uppAr[i] = upp;
+        lowAr[i] = low;
         upp++, low++;
-        //upp = 'A', low = 'a';
-        
-        
     }
 
-    // Check if the CLA is an integer
-    //int key = atoi(argv[1]); // Convert string to real integer
-    /*if (key < 1)
-    {
-        printf("Usage: ./substitution key\n");
-        return 1;
-    }*/
 
     // Get plaintext input from user and convert it to cipher using key.
-   /* string plain = get_string("plaintext:  ");
+    string plain = get_string("plaintext:  ");
     int n = strlen(plain);
-    int cipher;
+    char cipher[n];
     printf("ciphertext: ");
-    for (int i = 0; i < n; i++)
+    for (int k = 0; k < n; k++)
     {
-        if (plain[i] >= 'a' && plain[i] <= 'z')
+        if (plain[k] >= 'a' && plain[k] <= 'z')
         {
-            cipher = (((((int)plain[i] - 97) + 26) + key) % 26) + 97;
-            printf("%c", (char)cipher);
+            for (int l = 0; l < len; l++)
+            {
+                if (plain[k] == lowAr[l])
+                {
+                    printf("%c", tolower(key[l]));
+                }
+            }
+              
         }
-        else if (plain[i] >= 'A' && plain[i] <= 'Z')
+        else if (plain[k] >= 'A' && plain[k] <= 'Z')
         {
-            cipher = (((((int)plain[i] - 65) + 26) + key) % 26) + 65;
-            printf("%c", (char)cipher);
+                        for (int l = 0; l < len; l++)
+            {
+                if (plain[k] == uppAr[l])
+                {
+                    printf("%c", toupper(key[l]));
+                }
+            }
         }
-        // Print as is if it is not a letter.
-        else
+        else 
         {
-            cipher = (int)plain[i];
-            printf("%c", (char)cipher);
+            printf("%c", plain[k]);
         }
-
-    }*/
+    }
     printf("\n");
 
     return 0;
