777540fbad939e7446311981734f9ed8761c4a44
venzom <venzom@users.noreply.github.com>
2021-02-26 09:27:52 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -148,10 +148,10 @@ void add_pairs(void)
                 pairs[pair_count].loser = j;
                 pair_count++;
             }
-            else if (preferences[i][j] < preferences[j][j])
+            else if (preferences[i][j] < preferences[j][i])
             {
-                pairs[pair_count].winner = i;
-                pairs[pair_count].loser = j;
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
                 pair_count++;
             }
         }
