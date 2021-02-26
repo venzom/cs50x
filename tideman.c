67d74f32c7dd0f9ebe9be6282bf66e17d7c212ec
venzom <venzom@users.noreply.github.com>
2021-02-26 08:40:56 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -126,7 +126,7 @@ void record_preferences(int ranks[])
     // Create array of preferences for each candidate 
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = i + 1; j < candidate_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
             if (ranks[i] < ranks[j])
             {
