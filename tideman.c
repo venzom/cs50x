4a9776f2c3d823f2e6d2207c59e9e09faf349f4b
venzom <venzom@users.noreply.github.com>
2021-03-02 05:44:36 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -204,7 +204,7 @@ void sort_pairs(void)
 void lock_pairs(void)
 {
     // Populate i x j array with true of winner
-    // locked[pairs[0].winner][pairs[0].loser] = true;
+    locked[pairs[0].winner][pairs[0].loser] = true;
     // locked[pairs[1].winner][pairs[1].loser] = true;
     for (int i = 0; i < pair_count; i++)
         {
