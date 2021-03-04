1c65e3b1387d96bafca1b10afb89e0811f55125c
venzom <venzom@users.noreply.github.com>
2021-03-04 08:37:10 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -34,18 +34,17 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
     double r, g, b;
     
+    // Loop through each pixel in image
     for (int i = 0; i <= height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            // Calculate each new R, G r B value sepia color
+            // Calculate each new R, G r B value sepia color with provided formula
             r = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
             g = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
             b = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
-            r = round(r);
-            g = round(g);
-            b = round(b);
             
+            // Check value for each color is greater than 255(max), if so make it 255
             if (r > 255)
             {
                 r = 255;
@@ -59,9 +58,10 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
                 b = 255;
             }
             
-            image[i][j].rgbtRed = r;
-            image[i][j].rgbtGreen = g;
-            image[i][j].rgbtBlue = b;
+            // Round number and add replace in image
+            image[i][j].rgbtRed = round(r);
+            image[i][j].rgbtGreen = round(g);
+            image[i][j].rgbtBlue = round(b);
         }
     }
     return;
@@ -99,7 +99,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Create a duplicate of the image
+    // Create a duplicate of the image to take values from it to add to current image
     RGBTRIPLE dupl[height][width];
     
     for (int i = 0; i < height; i++)
@@ -110,45 +110,48 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         }
     }
     
+    // Loop through each pixel
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             int count = 0;
             float r = 0, g = 0, b = 0;
+            
+            // Loop through each surrounding pixel
+            // If surrounding pixel is outside the image, ignore
+            // If surrounding pixel is inside image, add to sum of each color(r, g, b)
             for (int k = -1; k < 2; k++)
             {
                 for (int l = -1; l < 2; l++)
                 {
-                    // Check if pixels are outside image
+                    // Check if pixels are outside image, top and bottom
                     if (i + k < 0 || i + k >= height)
                     {
                         continue;
                     }
+                    
+                    // Check if pixels are outside the image, left and right. 
                     if (j + l < 0 || j + l >= width)
                     {
                         continue;
                     }
+                    
+                    // Tally the pizels that will be added
                     count++;
+                    
+                    // Add up the pixels
                     r += dupl[i + k][j + l].rgbtRed;
                     g += dupl[i + k][j + l].rgbtGreen; 
                     b += dupl[i + k][j + l].rgbtBlue;
                 }
             }
+            
+            // Take the average of surrounding pixels and replace it in "image"
             image[i][j].rgbtRed = round(r / count);
             image[i][j].rgbtGreen = round(g / count);
             image[i][j].rgbtBlue = round(b / count);
         }
-        // for (int k = 0; k <= width; k++)
-        // {
-        //     image[i][k].rgbtRed = round(r[k] / count);
-        //     image[i][k].rgbtGreen = round(g[k] / count);
-        //     image[i][k].rgbtBlue = round(b[k] / count);
-            
-        //     r[k] = g[k] = b[k] = count = 0.0;
-            
-        // }
-        
     }
     return;
 }
