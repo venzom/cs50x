9994f137c2eb04c8dc9f50a6a11e3bda1ae3c124
venzom <venzom@users.noreply.github.com>
2021-03-05 14:12:31 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Read file in steps of 512 bytes at a time
+    // Define buffer for each byte
     BYTE buffer[512];
     char sBuffer[8];
     int count = 000;    
@@ -30,52 +30,39 @@ int main(int argc, char *argv[])
     // Read file 512 bytes at a time
     while (fread(&buffer, sizeof(BYTE), 512, card) != 0)
     {
-        
-        // Check first 4 bytes of file
+        // Check if the first 4 bytes of match JPEG format
         if ((buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
             && (buffer[3] >= 0xe0 && buffer[3] <= 0xef))
         {
-            
+            // Create new file name and open file
             sprintf(sBuffer, "%003i.jpg", count);
             FILE *name = fopen(sBuffer, "w");
-            // fwrite(&buffer, sizeof(BYTE), 512, name);
+            
+            // Continue writing until next 4 JPEG bytes are identified 
             do 
             {    
                 fwrite(&buffer, sizeof(BYTE), 512, name);
                 fread(&buffer, sizeof(BYTE), 512, card);
                 if (feof(card))
-            {
-                break;
-            }
+                {
+                    break;
+                }   
             }   
             while (!((buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff) 
-                && (buffer[3] >= 0xe0 && buffer[3] <= 0xef)));
-            fseek(card, -512, SEEK_CUR);    
-            fclose(name);
-            //check(*card);
-            count++;
+                     && (buffer[3] >= 0xe0 && buffer[3] <= 0xef)));
             
+            // Rewind 512 bytes to not skip images    
+            fseek(card, -512, SEEK_CUR);    
+            
+            // Close outfile
+            fclose(name);
+            
+            // Increase count for next file name
+            count++;
         }
-
     }
 
-
-    //free(buffer);
+    // Close infile
     fclose(card);
     return 0;
 }
-
-// void check(FILE card)
-// {
-//     sprintf(sBuffer, "%003i.jpg", count);
-//     FILE *name = fopen(sBuffer, "w");
-//     fwrite(&buffer, sizeof(BYTE), 512, name);
-//     do 
-//     {    
-        
-//         fwrite(&buffer, sizeof(BYTE), 512, name);
-//     }   
-//     while (!((buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff) 
-//         && (buffer[3] >= 0xe0 && buffer[3] <= 0xef)));
-//     fclose(name);
-// }
