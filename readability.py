97e559964ee98c16ef278e08d012952ded1d0248
venzom <venzom@users.noreply.github.com>
2021-03-11 10:38:57 +0000

    automated commit by submit50

--- readability.py
+++ readability.py
@@ -2,7 +2,7 @@ from cs50 import get_string
 
 # Get input text from user
 text = get_string("Text: ")
- 
+
 # Count sentences by counting punctuation
 sentences = text.count('!') + text.count('?') + text.count('.')
 
@@ -18,10 +18,6 @@ S = sentences * 100 / words
 index = 0.0588 * L - 0.296 * S - 15.8
 
 # Print out grade level index
-# print(letters)
-# print(words)
-# print(sentences)
-
 if index <= 1:
     print("Before Grade 1")
 elif index >= 16:
