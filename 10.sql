9bc2e3cc2253a868b3ac9d9644d6c27825bd7ec0
venzom <venzom@users.noreply.github.com>
2021-03-18 07:31:49 +0000

    automated commit by check50 [check50=True]

--- 10.sql
+++ 10.sql
@@ -2,4 +2,4 @@
 SELECT DISTINCT(name) FROM people
 JOIN directors ON people.id = directors.person_id
 JOIN ratings ON ratings.movie_id = directors.movie_id
-WHERE rating > 9.0;
+WHERE rating >= 9.0;
