afca81966b690a61e7f2c71b537e0b8d84615651
venzom <venzom@users.noreply.github.com>
2021-03-18 09:07:50 +0000

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -1,5 +1,10 @@
 /* List the titles of all movies in which both Johnny Depp and Helena Bonham Carted starred */
 SELECT title FROM people
 JOIN movies ON movies.id = stars.movie_id
-JOIN stars ON people.id = stars.person_id
-HAVING name IN ("Johnny Depp", "Helena Bonham Carter");
+JOIN stars ON stars.person_id = people.id
+WHERE name = "Johnny Depp"
+INTERSECT
+SELECT title FROM people
+JOIN movies ON movies.id = stars.movie_id
+JOIN stars ON stars.person_id = people.id
+WHERE name = "Helena Bonham Carter";
