2b9087890942cf9061153c99c5effb265f3aec1e
venzom <venzom@users.noreply.github.com>
2021-03-18 06:47:13 +0000

    automated commit by check50 [check50=True]

--- 7.sql
+++ 7.sql
@@ -1,8 +1,8 @@
 /* Query to list all movies released in 2010 and their ratings, in descending order by rating.
    For movies with the same rating, order alphabetically */
-SELECT title FROM movies
+SELECT movies.title, ratings.rating FROM movies
 JOIN ratings ON movies.id = ratings.movie_id
 WHERE year = 2010
-ORDER BY rating;
+ORDER BY rating DESC;
 
 
