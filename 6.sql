ef7beed9946f3531700c49106f7e5f14619052ff
venzom <venzom@users.noreply.github.com>
2021-03-18 05:04:19 +0000

    automated commit by check50 [check50=True]

--- 6.sql
+++ 6.sql
@@ -1,2 +1,2 @@
 /* List the names of songs that are by post malone */
-SELECT name FROM songs WHERE id IN (SELECT id FROM artists WHERE name = "Post Malone");
+SELECT name FROM songs WHERE artist_id IN (SELECT id FROM artists WHERE name = "Post Malone");
