9cbb2334b787a80e2ffd3a24c413675ef3fce0bb
venzom <venzom@users.noreply.github.com>
2021-03-18 22:08:45 +0000

    automated commit by submit50

--- 13.sql
+++ 13.sql
@@ -10,22 +10,3 @@ WHERE person_id IN
 WHERE name = "Kevin Bacon" AND birth = 1958)))
 AND NOT name = "Kevin Bacon"
 GROUP BY people.id;
-
-
--- SELECT name FROM movies
--- JOIN people ON people.id = stars.person_id AND stars.person_id =
--- (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958)
--- JOIN stars ON movies.id = stars.movie_id
-
--- INTERSECT
--- SELECT stars FROM movies
--- SELECT name FROM movies
--- JOIN stars ON people.id = stars.person_id
--- JOIN movies ON movies.id = stars.movie_id
--- WHERE movie_id = (SELECT movie_id FROM stars WHERE person_id =
--- (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958));
-
--- SELECT name FROM people WHERE id =
--- (SELECT person_id FROM stars WHERE movie_id =
--- (SELECT movie_id FROM stars WHERE person_id =
--- (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958)));
