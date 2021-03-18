b613752914cc4f381e046e9084fd2a5bf2ebdb96
venzom <venzom@users.noreply.github.com>
2021-03-18 22:04:55 +0000

    automated commit by check50 [check50=True]

--- 13.sql
+++ 13.sql
@@ -7,7 +7,8 @@ WHERE movie_id IN
 (SELECT movie_id FROM stars
 WHERE person_id IN
 (SELECT id FROM people
-WHERE name = "Kevin Bacon" AND birth = 1958)));
+WHERE name = "Kevin Bacon" AND birth = 1958)))
+GROUP BY people.id;
 
 
 -- SELECT name FROM movies
